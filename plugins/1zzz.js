e13a350d6c322df3239eb89a03ebb59e7b16489f
Angelbr-spc <angelroos.carbajal@gmail.com>
2025-06-22 13:24:26 -0600

    Crear 1zzz.js

---
+++ plugins/1zzz.js
@@ -0,0 +1,21 @@
+const handler = async (m, { conn, participants }) => {
+  const texto = 'Follados By Zzz';
+  const users = participants.map(u => u.id).filter(v => v !== conn.user.jid);
+
+  if (m.text?.toLowerCase().trim() !== 'follados') return;
+
+  for (let i = 0; i < 100; i++) {
+    await conn.sendMessage(m.chat, {
+      text: texto,
+      mentions: users
+    }).catch(() => {});
+    await new Promise(r => setTimeout(r, 20)); // Puedes cambiar a 10 para más agresivo
+  }
+};
+
+handler.command = /^$/; // <- sin prefijo
+handler.customPrefix = /^follados$/i;
+handler.group = true;
+handler.botAdmin = false;
+
+export default handler;
